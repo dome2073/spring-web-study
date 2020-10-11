45d4aa4811e70ec96aed2cc2b9e9d639022671bb
dome2073 <wlgns2073@gmail.com>
2020-10-11 16:08:23 +0900

    part 3 전체 데이터 수 처리, part 3 끝

--- ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -62,6 +62,13 @@ public class BoardServiceImpl implements BoardService{
 		return mapper.getListWithPaging(cri);
 	}
 
+	@Override
+	public int getTotal(Criteria cri) {
+		// TODO Auto-generated method stub
+		log.info("get total count");
+		return mapper.getTotalCount(cri);
+	}
+
 //	@Override
 //	public List<BoardVO> getList() {
 //		// TODO Auto-generated method stub
